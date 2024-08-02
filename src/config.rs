3a0db3f31d5090f62dd6b83df7ef3d5711435293
Steven He <hess1@uci.edu>
2024-08-02 13:44:31 -0700

    Load config/default config

---
+++ src/config.rs
@@ -0,0 +1,65 @@
+use home::home_dir;
+use serde::{Deserialize, Serialize};
+use serde_json;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+
+#[derive(Serialize, Deserialize)]
+pub struct Config {
+    pub compile_command: String,
+    pub compile_args: Vec<String>,
+    pub contest_directory: String,
+}
+
+pub fn load_config() -> Config {
+    if let Some(home_path) = home_dir() {
+        let mut config_path = PathBuf::from(home_path);
+        config_path.push("cpx.json");
+
+        if let Ok(data) = fs::read_to_string(config_path) {
+            if let Ok(local_cfg) = serde_json::from_str(&data) {
+                return local_cfg;
+            }
+        }
+    }
+
+    default_config()
+}
+
+fn default_config() -> Config {
+    let default = Config {
+        compile_command: "g++".into(),
+        compile_args: vec![
+            "solution.cpp".into(),
+            "-o".into(),
+            "cpp.out".into(),
+            "-std=c++17".into(),
+        ],
+        contest_directory: {
+            if let Some(dir) = home_dir() {
+                format!("{}", dir.display())
+            } else {
+                "/".into()
+            }
+        },
+    };
+
+    if let Some(home_path) = home_dir() {
+        let mut config_path = PathBuf::from(home_path);
+        config_path.push("cpx.json");
+
+        let json = serde_json::to_string_pretty(&default).expect("Uh oh");
+        let mut file = fs::File::create(&config_path).expect("Uh oh");
+
+        match file.write_all(json.as_bytes()) {
+            Ok(_) => println!("Writing default config to {}", config_path.display()),
+            Err(_) => eprintln!(
+                "Failed to write default config to {}",
+                config_path.display()
+            ),
+        }
+    }
+
+    default
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use clap::{Parser, Subcommand};
+use core::str;
+use std::process::Command;
+
+mod config;
+use config::{load_config, Config};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
-    cmd: Command,
+    cmd: Cmd,
 }
 
 #[derive(Subcommand)]
-enum Command {
+enum Cmd {
     Listen,
     Build,
     Run { test_number: Option<i32> },
@@ -17,12 +22,25 @@ enum Command {
 fn main() {
     let args = Cli::parse();
 
+    // Load or create config
+    let user_cfg: Config = load_config();
+
     match &args.cmd {
-        Command::Listen => {}
-        Command::Build => {}
-        Command::Run { test_number } => match test_number {
+        Cmd::Listen => {}
+        Cmd::Build => build(&user_cfg),
+        Cmd::Run { test_number } => match test_number {
             Some(number) => println!("Running test #{}", number),
             None => println!("Running all tests"),
         },
     }
+}
+
+fn build(cfg: &Config) {
+    let build_cmd = Command::new(&cfg.compile_command)
+        .args(&cfg.compile_args)
+        .output()
+        .unwrap();
+
+    let stdout = str::from_utf8(&build_cmd.stdout).unwrap();
+    println!("{}", stdout);
 }
